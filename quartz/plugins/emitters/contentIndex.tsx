0f3dfb8fcc7af57cdf8b11489114fbb94e32afc3
긍정우_macbook <2020231144@yonsei.ac.kr>
2026-01-09 17:03:27 +0900

    }누락

--- quartz/plugins/emitters/contentIndex.tsx
+++ quartz/plugins/emitters/contentIndex.tsx
@@ -53,7 +53,7 @@ function generateSiteMap(cfg: GlobalConfiguration, idx: ContentIndexMap): string
   return `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${urls}
-  </urlset>`
+  </urlset>`}
 
 function generateRSSFeed(cfg: GlobalConfiguration, idx: ContentIndexMap, limit?: number): string {
   const base = cfg.baseUrl ?? ""
